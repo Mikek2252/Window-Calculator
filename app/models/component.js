8c16a9ad30734f6f439628897231c31b68d936ef
mikek2252 <Michael.r.kerr1@gmail.com>
2021-01-08 15:31:11 +0000

    add cubic feet to component and model

--- app/models/component.js
+++ app/models/component.js
@@ -1,5 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 
+const MM_TO_FEET = 305;
+
 export default class ComponentModel extends Model {
 
   @attr name;
@@ -8,5 +10,12 @@ export default class ComponentModel extends Model {
   @attr width;
   @attr depth;
   @attr count;
+
+  get cubicFeet() {
+    let heightInFeet = ((this.height * 100) / MM_TO_FEET) / 100;
+    let widthInFeet = ((this.width * 100) / MM_TO_FEET) / 100;
+    let depthInFeet = ((this.depth * 100) / MM_TO_FEET) / 100;
+    return heightInFeet * widthInFeet * depthInFeet;
+  }
 
 }
